40c77f580101d0dd10d8c6c1991da674bee26dbf
a827415225 <827415225@qq.com>
2018-04-17 17:35:36 +0800

    add 19·

--- 19生成器.py
+++ 19生成器.py
@@ -71,9 +71,11 @@ print(f)
             
 def triangles():
     L=[1]
-    while len(L) < n:
+    n
+    while True:
         yield L
-        L = [1] + [L[x-1]+L[x] for x in range(len(L))] + [1]
+        L.append(0)
+        L = [L[x-1]+L[x] for x in range(len(L))]
 
 # 期待输出:
 # [1]
@@ -94,6 +96,9 @@ for t in triangles():
     n = n + 1
     if n == 10:
         break
+
+print(results)
+
 if results == [
     [1],
     [1, 1],
@@ -106,6 +111,7 @@ if results == [
     [1, 8, 28, 56, 70, 56, 28, 8, 1],
     [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
 ]:
+
     print('测试通过!')
 else:
     print('测试失败!')
